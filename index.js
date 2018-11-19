93c1520c7cf26847f111ced40e83de0daaff40c8
alpha-constant-7616 <anderson-tomas@hotmail.com>
2018-11-19 18:18:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // add solution here
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 var pairs = [];
 function theBeatlesPlay(musician, instrument){
 
-  var i = 0;
+  var i;
   for(i = 0; i < musician; i++){
     pairs[i] = musician[i] + "plays" + instrument[i];
     i++;
