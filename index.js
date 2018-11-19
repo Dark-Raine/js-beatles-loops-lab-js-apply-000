111d7efca0a54bb9eafde82625afae8c324fb6c7
alpha-constant-7616 <anderson-tomas@hotmail.com>
2018-11-19 18:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   const pairs = []
-  for(const i = 0; i < musicians; i++){
+  for(var i = 0; i < musicians; i++){
     pairs[i] = musicians[i] + "plays" + instruments[i]
   }
   return pairs
