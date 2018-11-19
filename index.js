d91639ed48a66e560638b31820c5768c8ad016b8
alpha-constant-7616 <anderson-tomas@hotmail.com>
2018-11-19 18:18:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musician, instrument){
 
   var i;
   for(i = 0; i < musician; i++){
-    pairs[i] = musician[i] + "plays" + instrument[i];
+    pairs[i] = musician[i] + " plays " + instrument[i];
     i++;
   }
   return pairs;
