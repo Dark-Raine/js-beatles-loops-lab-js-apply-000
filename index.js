4f98318af2dcd4eb463df58b9faf639a0dd149d4
alpha-constant-7616 <anderson-tomas@hotmail.com>
2018-11-19 17:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  const pairs = []
+  for(i = 0; i < musicians; i++){
+    pairs[i] = musicians[i] + "plays" + instruments[i]
+  }
+}
