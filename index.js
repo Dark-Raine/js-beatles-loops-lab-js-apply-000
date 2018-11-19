86b2587f426abf43c86c47ae0f36ffe5666a4b53
alpha-constant-7616 <anderson-tomas@hotmail.com>
2018-11-19 18:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments){
-  const pairs = []
+  const pairs = [];
   for(var i = 0; i < musicians; i++){
-    pairs[i] = musicians[i] + "plays" + instruments[i]
+    pairs[i] = musicians[i] + "plays" + instruments[i];
   }
-  return pairs
+  return pairs;
 }
